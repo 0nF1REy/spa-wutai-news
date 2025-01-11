ca9bbd5a8c7842244d6df18c959a23d3b9041d2c
Alan Ryan <alanryan619@gmail.com>
2025-01-11 17:24:56 -0300

    feat: create data mock

---
+++ src/Datas.js
@@ -0,0 +1,27 @@
+export const news = [
+    {
+        title: "Notícia 01 | Em Chicago, a Noite é da Rally Vincent: A Caçadora de Recompensas Implacável",
+        text: "Mergulhe nas ruas sombrias de Chicago, onde o crime espreita em cada esquina e a noite guarda seus próprios segredos. Aqui, a caçadora de recompensas Rally Vincent domina o cenário, uma figura imponente com sua mira precisa e sua paixão por carros clássicos. Gunsmith Cats nos leva para um mundo noir, com sua atmosfera densa e personagens complexos. Rally, com sua postura séria e seu olhar penetrante, é uma anti-heroína que luta contra o crime com determinação e sem piedade. Sua Shelby GT500, uma máquina poderosa e veloz, é sua principal aliada nas perseguições em alta velocidade que marcam suas aventuras. Cada caso que Rally aceita a leva a confrontos com criminosos perigosos e conspirações complexas, desafiando sua habilidade e sua inteligência. Sua personalidade forte e reservada esconde um coração gentil, mas ela não hesita em usar a violência quando necessário para cumprir seus objetivos. Acompanhe Rally em suas caçadas implacáveis, onde o perigo é constante e a sobrevivência depende de suas habilidades e de sua determinação. Em um mundo onde a moralidade é ambígua, Rally Vincent se destaca como uma força da natureza, navegando pelas ruas escuras de Chicago com a mesma precisão com que dispara sua arma. Gunsmith Cats é uma jornada envolvente que explora a complexidade da natureza humana em meio a um ambiente repleto de sombras e segredos.",
+        image: "https://miro.medium.com/v2/resize:fit:1400/1*ddyz8qnOhFeFKY-_c3tleQ.jpeg",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title:
+            "Notícia 02 | Irene 'Minnie' Hopkin: A Mestre em Explosivos que Traz Caos (e Resultados) para Chicago",
+        text: "No submundo de Chicago, onde a sutileza muitas vezes falha, existe uma especialista em explosivos que causa impacto: Irene 'Minnie' Hopkin. Com sua aparência jovial que esconde um conhecimento técnico profundo, Minnie é a arma secreta em muitas das operações mais arriscadas da cidade. Seja desmontando bombas, criando armadilhas engenhosas ou simplesmente explodindo barreiras, Minnie faz tudo com um sorriso no rosto e uma precisão de tirar o fôlego. Sua personalidade excêntrica e seu amor por tudo que explode fazem dela uma figura inesquecível, e seus métodos não convencionais a tornam um trunfo valioso para seus aliados. Mas não se engane pela sua alegria; Minnie é uma profissional quando o assunto é lidar com explosivos, sempre atenta aos detalhes e com um timing perfeito. Se você se encontrar no caminho dela, ou do que ela quer explodir, é melhor correr, pois Minnie não costuma dar segundas chances. A cidade treme quando Minnie Hopkin está por perto e com razão, afinal, ela é a prova de que um pouco de caos pode trazer resultados surpreendentes.",
+        image:
+            "https://miro.medium.com/v2/resize:fit:1400/1*ddyz8qnOhFeFKY-_c3tleQ.jpeg",
+        likes: 5,
+        comments: 10,
+    },
+    {
+        title:
+            "Notícia 03 | 'O Bicho-Papão' de Chicago: O Inconfundível Ken Takizawa e sua Luta Incansável Contra o Crime",
+        text: "Nas sombras da metrópole de Chicago, um homem paira como uma lenda: Ken Takizawa, conhecido por alguns como 'O Bicho-Papão'. Com sua destreza em combate e sua lealdade inabalável, Ken é uma força a ser reconhecida. Ex-oficial da polícia, ele agora opera fora da lei, caçando criminosos com uma intensidade e determinação que poucos podem igualar. Sua habilidade em artes marciais e seu conhecimento tático o tornam um adversário formidável, e sua busca incansável por justiça o leva a confrontar os elementos mais sombrios da cidade. Embora seu comportamento silencioso e muitas vezes frio possa intimidar, Ken possui um forte senso de moralidade e uma determinação inabalável em proteger os inocentes. Ele é um dos poucos a se posicionar contra a crescente onda de violência, um guardião sombrio que caminha entre a luz e as trevas. Se você cruzar com Ken, você pode ter certeza de que a justiça virá, por bem ou por mal.",
+        image:
+            "https://miro.medium.com/v2/resize:fit:1400/1*ddyz8qnOhFeFKY-_c3tleQ.jpeg",
+        likes: 7,
+        comments: 2,
+    },
+];
