bc1f7a0e82da1301c720d8070a7fc619cea7ed3d
Alan Ryan <alanryan619@gmail.com>
2025-01-12 17:40:39 -0300

    feat: implemented styled components and applied CSS styles to the navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,18 @@
 import logo from "../../images/LogoWN.png";
-import "./Navbar.css"
+import { Button, ImageLogo, InputSpace, Nav } from "./NavbarStyled";
 
 export function Navbar() {
   return (
     <>
-      <nav>
-        <div className="input-search-space">
+      <Nav>
+        <InputSpace>
           <i className="bi bi-search"></i>
           <input type="text" placeholder="Pesquise por um título" />
-        </div>
-        <img src={logo} alt="Logo do Wutai News" />
+        </InputSpace>
+        <ImageLogo src={logo} alt="Logo do Wutai News" />
 
-        <button>Entrar</button>
-      </nav>
+        <Button>Entrar</Button>
+      </Nav>
     </>
   );
 }
